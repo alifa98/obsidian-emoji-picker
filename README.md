27e8812df8c6a896e06bc6136d785654eb211859
Ali Faraji <31096694+alifa98@users.noreply.github.com>
2024-11-09 03:30:41 -0500

    Emoji Picker plugin

---
+++ README.md
@@ -0,0 +1,24 @@
+# Emoji Picker plugin for Obsidian
+
+This plugin adds a button to the editor toolbar that opens a modal with a searchable emoji picker. Clicking an emoji inserts it at the cursor position in the editor.
+
+You can also set a hotkey to open the emoji picker. 
+The recommended hotkey is `Ctrl+.`, but you can set it to any key combination you like.
+
+## Emoji Version
+
+As of November 2024, we are using the emoji-mart library so the emoji version is [14](https://emojipedia.org/emoji-14.0/).
+
+
+## Installation
+
+### From within Obsidian
+- Open Settings > 
+
+### Manual installation
+
+- Download the latest release from the [releases page](https://github.com/alifa98/obsidian-emoji-picker/releases).
+- Copy over `main.js`, `styles.css`, `manifest.json` to your vault `VaultFolder/.obsidian/plugins/emoji-picker-obsidian/`.
+- Reload Obsidian.
+- Enable the plugin in the settings.
+- Set a hotkey for the plugin in the settings.
---
+++ main.ts
@@ -0,0 +1,159 @@
+import { App, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import data from '@emoji-mart/data'
+import Picker from '@emoji-mart/react'
+import { createElement } from 'react';
+import { Root, createRoot } from 'react-dom/client';
+
+interface EmojiPickerPluginSetting {
+	showEmojiButton: boolean;
+}
+
+const DEFAULT_SETTINGS: EmojiPickerPluginSetting = {
+	showEmojiButton: true
+}
+
+export default class EmojiPickerPlugin extends Plugin {
+	settings: EmojiPickerPluginSetting;
+	emojiButton: HTMLElement;
+
+
+	showEmojiPicker() {
+		const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+		// Check if view is in editing mode
+		if (markdownView && markdownView.getMode() === 'source') {
+			const modal = new EmojiPickerModal(this.app);
+			modal.open();
+		} else {
+			new Notice('Emoji picker can only be opened in editing mode.');
+		}
+	}
+
+	addRibbonButton() {
+		const ribbonIconEl = this.addRibbonIcon('smile', 'Open Emoji Picker', async (evt: MouseEvent) => {
+			this.showEmojiPicker();
+		});
+		this.emojiButton = ribbonIconEl;
+	}
+
+	async onload() {
+		await this.loadSettings();
+
+		// This creates an icon in the left to open a modal for the emoji picker.
+		if (this.settings.showEmojiButton) {
+			this.addRibbonButton();
+		}
+
+		this.addCommand({
+			id: 'open-emoji-picker',
+			name: 'Open Emoji Picker',
+			checkCallback: (checking: boolean) => {
+				// Conditions to check
+				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+				if (markdownView) {
+					// If checking is true, we're simply "checking" if the command can be run.
+					// If checking is false, then we want to actually perform the operation.
+					if (!checking) {
+						this.showEmojiPicker();
+					}
+
+					// This command will only show up in Command Palette when the check function returns true
+					return true;
+				}
+			}
+		});
+
+		// This adds a settings tab so the user can configure various aspects of the plugin
+		this.addSettingTab(new EmojiPickerSettingsTab(this.app, this));
+
+	}
+
+	onunload() {
+
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+}
+
+class EmojiPickerModal extends Modal {
+
+	root: Root;
+
+	constructor(app: App) {
+		super(app);
+
+		// Remove the background and modal restrictions
+		this.containerEl.id = 'emoji-picker-modal';
+	}
+
+	onOpen() {
+		const picker = createElement(Picker, {
+			data: data,
+			onEmojiSelect: (emoji: any) => {
+				const editor = this.app.workspace.getActiveViewOfType(MarkdownView)?.editor;
+				if (editor) {
+					const cursor = editor.getCursor('from');
+
+					// Insert emoji at the current cursor position
+					editor.replaceRange(emoji.native, cursor, cursor);
+
+					// Move the cursor to the position right after the inserted emoji
+					const newCursor = {
+						line: cursor.line,
+						ch: cursor.ch + emoji.native.length
+					};
+					editor.setCursor(newCursor);
+
+					editor.focus();
+				}
+				this.close();
+			},
+			skinTonePosition: 'search',
+		});
+
+		this.root = createRoot(this.containerEl.children[1]);
+		this.root.render(
+			picker
+		);
+	}
+
+	onClose() {
+		const { contentEl } = this;
+		contentEl.empty();
+	}
+}
+
+class EmojiPickerSettingsTab extends PluginSettingTab {
+	plugin: EmojiPickerPlugin;
+
+	constructor(app: App, plugin: EmojiPickerPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const { containerEl } = this;
+
+		containerEl.empty();
+
+		new Setting(containerEl)
+			.setName('Show emoji button')
+			.setDesc('Show the emoji button in the left ribbon')
+			.addToggle(toggle => toggle
+				.setValue(this.plugin.settings.showEmojiButton)
+				.onChange(async (value) => {
+					if (value) {
+						this.plugin.addRibbonButton();
+					} else {
+						this.plugin.emojiButton.remove();
+					}
+					this.plugin.settings.showEmojiButton = value;
+					await this.plugin.saveSettings();
+				}));
+	}
+}
